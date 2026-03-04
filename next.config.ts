3ac51b28a045f2b3ca31704590dce64804278c73
Serizona <serenabusceti@hotmail.com>
2026-03-04 10:48:15 +0100

    Add redirect from intusai.com to www.intusai.com

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,17 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   compress: true,
 
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [{ type: 'host', value: 'intusai.com' }],
+        destination: 'https://www.intusai.com/:path*',
+        permanent: true,
+      },
+    ];
+  },
+
   // Abilita compressione per file statici
   async headers() {
     return [
